4a6d258dca1e8dfe8ab4765b23a08d717f583253
mgboyle <57579450+mgboyle@users.noreply.github.com>
2023-04-03 08:43:19 -0400

    Use perf counter instead

--- DotNetExporterTest/ConsoleApp1/ConsoleApp1/Program.cs
+++ DotNetExporterTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Prometheus;
@@ -19,31 +18,29 @@ namespace MyNamespace
             // create a gauge to track the total CPU usage of the system
             var cpuUsageTotal = Metrics.CreateGauge("cpu_usage_total", "Total CPU usage of the system");
 
-            // create a gauge to track the available disk space for each drive
-            var diskSpaceAvailable = Metrics.CreateGauge("disk_space_available", "Available disk space for each drive", new GaugeConfiguration
-            {
-                LabelNames = new[] { "drive" }
-            });
-
-            // create a gauge to track the I/O read time for each process
-            var ioReadTime = Metrics.CreateGauge("io_read_time", "I/O read time for each process", new GaugeConfiguration
+            // create a gauge to track the CPU usage of the top 5 processes by CPU usage
+            var cpuUsageTop5 = Metrics.CreateGauge("cpu_usage_top_5", "CPU usage of the top 5 processes by CPU usage", new GaugeConfiguration
             {
                 LabelNames = new[] { "process_name" }
             });
+
+            // create a PerformanceCounter to track the total CPU usage of the system
+            PerformanceCounter totalCpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
 
             // start a background task to update the metrics
             Task.Run(() =>
             {
                 while (true)
                 {
-                    // get the total CPU time used by all processes on the system
-                    var cpuTime = new TimeSpan();
+                    // update the total CPU usage gauge using the PerformanceCounter
+                    cpuUsageTotal.Set(totalCpuCounter.NextValue());
+
+                    // get the top 5 processes by CPU usage
                     var processes = new List<Process>();
                     foreach (var process in Process.GetProcesses())
                     {
                         try
                         {
-                            cpuTime += process.TotalProcessorTime;
                             processes.Add(process);
                         }
                         catch
@@ -52,29 +49,31 @@ namespace MyNamespace
                         }
                     }
 
-                    // calculate the CPU usage as a percentage of the total available CPU capacity on the system
-                    var totalCpuTime = Environment.TickCount * Environment.ProcessorCount;
-                    var cpuUsagePercentageTotal = (float)(cpuTime.TotalMilliseconds / totalCpuTime);
-
-                    // update the total CPU usage gauge
-                    cpuUsageTotal.Set(cpuUsagePercentageTotal);
-
-                    // update the available disk space gauge for each drive
-                    foreach (var drive in DriveInfo.GetDrives())
-                    {
-                        if (drive.IsReady)
-                        {
-                            diskSpaceAvailable.WithLabels(drive.Name).Set(drive.AvailableFreeSpace);
-                        }
-                    }
-
-                    // update the I/O read time gauge for each process
-                    foreach (var process in processes)
+                    var top5Processes = processes.OrderByDescending(p =>
                     {
                         try
                         {
-                            var ioCounter = new PerformanceCounter("Process", "IO Read Bytes/sec", process.ProcessName, true);
-                            ioReadTime.WithLabels(process.ProcessName).Set(ioCounter.NextValue());
+                            using (PerformanceCounter processCpuCounter = new PerformanceCounter("Process", "% Processor Time", p.ProcessName))
+                            {
+                                return processCpuCounter.NextValue();
+                            }
+                        }
+                        catch
+                        {
+                            return 0.0f;
+                        }
+                    }).Take(5);
+
+                    // update the CPU usage gauge for each of the top 5 processes
+                    foreach (var process in top5Processes)
+                    {
+                        try
+                        {
+                            using (PerformanceCounter processCpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName))
+                            {
+                                var cpuUsagePercentageTop5 = processCpuCounter.NextValue();
+                                cpuUsageTop5.WithLabels(process.ProcessName).Set(cpuUsagePercentageTop5);
+                            }
                         }
                         catch
                         {
